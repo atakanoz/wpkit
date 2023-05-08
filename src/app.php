d52470b01290a7180f3575da737667b7255fbc8c
Atakan Öz <atakanozofficial@gmail.com>
2023-05-08 22:55:36 +0300

    file cases

---
+++ src/app.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace Theme;
+
+class Kit {
+
+	/**
+	 * Theme name.
+	 *
+	 * @var string $name.
+	 */
+	private static $name;
+
+	/**
+	 * Theme version.
+	 *
+	 * @var string $version.
+	 */
+	public static $version;
+
+	/**
+	 * Icon directory.
+	 *
+	 * @var string $icon_dir.
+	 */
+	public static $icon_dir;
+
+	/**
+	 * Image directory.
+	 *
+	 * @var string $image_dir;
+	 */
+	public static $image_dir;
+
+	/**
+	 * Styles.
+	 *
+	 * @var array $styles.
+	 */
+	public static $styles = array();
+
+	/**
+	 * Scripts
+	 *
+	 * @var array $scripts.
+	 */
+	public static $scripts = array();
+
+	/**
+	 * JQuery allowance.
+	 *
+	 * @var string $jquery.
+	 */
+	public static $jquery;
+
+	/**
+	 * Custom fields.
+	 *
+	 * @var string $custom_fields;
+	 */
+	public static $custom_fields;
+
+	/**
+	 * Sidebars.
+	 *
+	 * @var array $sidebars;
+	 */
+	public static $sidebars = array();
+
+	/**
+	 * Menus.
+	 *
+	 * @var array $menus.
+	 */
+	public static $menus = array();
+
+	/**
+	 * Include Files.
+	 *
+	 * @var string $composer.
+	 */
+	protected $include_files;
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 * @var \Noma\Plugin The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 * @return \Noma\Plugin An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance->run();
+
+	}
+
+	public function run() {
+		/**
+		 * Include Theme's setup file.
+		 * This particular file contains configuration for the themes.
+		 */
+		require get_template_directory() . '/includes/setup.php';
+
+		/**
+		 * Initiate a new Setup.
+		 */
+		$setup_module    = new Setup();
+		$security_module = new Security();
+
+		/**
+		 * Make Setup.
+		 */
+		\add_action( 'widgets_init', array( $setup_module, 'make_sidebars' ) );
+		\add_action( 'wp_enqueue_scripts', array( $setup_module, 'styles' ) );
+		\add_action( 'wp_enqueue_scripts', array( $setup_module, 'scripts' ) );
+		\add_action( 'after_setup_theme', array( $setup_module, 'make_nav_menus' ) );
+	}
+
+	/**
+	 * Debug.
+	 *
+	 * @param  mixed $code
+	 * @return void
+	 */
+	public static function debug( $code ) {
+
+		Helpers::get_debugger( $code );
+
+	}
+
+	/**
+	 * Layout.
+	 *
+	 * @var  mixed $layout_name
+	 * @param  mixed $layout_content
+	 * @param  mixed $layout_args
+	 * @return void
+	 */
+	public static function layout( $layout_name, $layout_content, $layout_args = array() ) {
+
+		Helpers::get_layout( $layout_name, $layout_content, $layout_args );
+
+	}
+
+	/**
+	 * Icon.
+	 *
+	 * @param  mixed $icon
+	 * @return void
+	 */
+	public static function icon( $icon = '' ) {
+
+		Helpers::get_icon( $icon );
+
+	}
+
+	/**
+	 * Image.
+	 *
+	 * @param  mixed $image
+	 * @param  mixed $alt
+	 * @param  mixed $class
+	 * @return void
+	 */
+	public static function image( $image, $alt = '', $class = 'x-image', $type = 'tag' ) {
+
+		Helpers::get_image( $image, $alt, $class, $type );
+
+	}
+
+	/**
+	 * Template.
+	 *
+	 * @param  mixed $template
+	 * @param  mixed $args
+	 * @return void
+	 */
+	public static function template( $template, $args = array() ) {
+
+		Helpers::get_template( $template, $args );
+
+	}
+
+	/**
+	 * Button.
+	 *
+	 * @param  mixed $content
+	 * @param  mixed $link
+	 * @param  mixed $class
+	 * @param  mixed $style
+	 * @return void
+	 */
+	public static function button( $content, $link = '#', $class = '', $style = '' ) {
+
+		Helpers::get_button( $content, $link, $class, $style );
+
+	}
+
+	/**
+	 * Init.
+	 *
+	 * @return void
+	 */
+	public static function init( $init = array() ) {
+
+		// Fill class constants.
+		self::$name          = $init['theme_name'];
+		self::$version       = $init['theme_version'];
+		self::$icon_dir      = get_stylesheet_directory() . $init['icon_directory'];
+		self::$image_dir     = get_stylesheet_directory_uri() . $init['image_directory'];
+		self::$styles        = $init['styles'];
+		self::$scripts       = $init['scripts'];
+		self::$custom_fields = $init['custom_fields'];
+		self::$sidebars      = $init['sidebars'];
+		self::$menus         = $init['menus'];
+		self::$jquery        = $init['jquery_support'] ? array( 'jquery' ) : array();
+		self::$custom_fields = $init['custom_fields'] ? $init['custom_fields'] : 'none';
+
+		/**
+		 * Initialize Modules.
+		 *
+		 * @since 1.0.0
+		 */
+		foreach ( $init['include_files'] as $filename ) {
+			require get_template_directory() . '/includes/' . $filename;
+		}
+	}
+
+}
---
+++ src/setup.php
@@ -0,0 +1,125 @@
+<?php
+namespace Theme;
+
+/**
+ * Setup Extender.
+ * -------------------------------------------------------------
+ * This class is mainly crafted for to create required setup
+ * methods to be used with the Kit class.
+ *
+ * These methods are linked at class-kit.php and called with
+ * Kit namespace (e.g Kit::method()).
+ *
+ * @category   Extender
+ * @package    SiteKit
+ * @version    1.0.0
+ * @author     KitStudio <hello@kitstudio.com>
+ * @copyright  2022 KitStudio
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @see        Kit, Kit::icon(), Kit::layout() etc...
+ */
+class Setup extends Kit {
+
+	/**
+	 * Theme Styles.
+	 *
+	 * @since 1.0.0
+	 * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+	 */
+	public function styles() {
+
+		if ( is_array( Kit::$styles ) ) {
+			foreach ( Kit::$styles as $style => $path ) {
+				wp_enqueue_style( $style, get_stylesheet_directory_uri() . $path, array(), Kit::$version );
+			}
+		}
+	}
+
+	/**
+	 * Theme Scripts.
+	 *
+	 * @since 1.0.0
+	 * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+	 */
+	public function scripts() {
+		if ( is_array( Kit::$scripts ) ) {
+			foreach ( Kit::$scripts as $script => $path ) {
+				wp_enqueue_script( $script, get_stylesheet_directory_uri() . $path, Kit::$jquery, Kit::$version, true );
+			}
+		}
+	}
+
+	/**
+	 * Initialize Carbon Fields.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function init_carbon_fields() {
+
+		if ( Kit::$custom_fields === 'carbon_fields' && Kit::$composer === true ) {
+
+			\Carbon_Fields\Carbon_Fields::boot();
+
+		}
+	}
+
+	/**
+	 * Sidebars.
+	 *
+	 * @param array $sidebars Sidebars array.
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function make_sidebars() {
+
+		if ( is_array( Kit::$sidebars ) || is_object( Kit::$sidebars ) ) {
+
+			foreach ( Kit::$sidebars as $sidebar => $value ) {
+
+				/**
+				 * Register the sidebars.
+				 *
+				 * @link https://developer.wordpress.org/reference/functions/register_sidebar/
+				 */
+				register_sidebar(
+					array(
+						'id'            => $sidebar,
+						'name'          => $value['name'],
+						'description'   => isset( $value['description'] ) ? $value['description'] : '',
+						'before_widget' => isset( $value['before_widget'] ) ? $value['before_widget'] : '',
+						'after_widget'  => isset( $value['after_widget'] ) ? $value['after_widget'] : '',
+						'before_title'  => isset( $value['before_title'] ) ? $value['before_title'] : '',
+						'after_title'   => isset( $value['after_title'] ) ? $value['after_title'] : '',
+					)
+				);
+			}
+		}
+	}
+
+	/**
+	 * Nav Menus.
+	 *
+	 * @since 1.0.0
+	 * @param  mixed $menus
+	 * @return void
+	 */
+	public function make_nav_menus() {
+		/**
+		 * Register the navigation menus.
+		 *
+		 * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+		 */
+
+		if ( is_array( Kit::$sidebars ) || is_object( Kit::$sidebars ) ) {
+
+			foreach ( Kit::$menus as $location => $description ) {
+
+				register_nav_menu( $location, $description );
+
+			}
+		}
+
+	}
+
+}
